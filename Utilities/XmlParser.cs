5cc89ece8b041810231d85cc0ec72eb6f6597e01
cdnicoll <cdnicoll@localhost>
2010-12-30 10:27:36 -0800

    Added a Utility library that handles extensions and XML libraries

---
+++ Utilities/XmlParser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Utilities
+{
+    public class XmlParser
+    {
+
+        private XmlDocument xDoc;
+        public string FileName { get; set; }
+
+        public XmlParser(string filename)
+        {
+            FileName = filename;
+            xDoc = new XmlDocument();
+            xDoc.Load(FileName);
+
+        }
+
+
+        /// <summary>
+        /// get all node values of a specific node name
+        /// </summary>
+        /// <param name="nodeName">name of node to fetch</param>
+        public XmlNodeList getNodeValues(string nodeName)
+        {
+           XmlNodeList nodes = xDoc.GetElementsByTagName(nodeName);
+           //List<String> node = nodes.InnerHtml;
+            return nodes;
+        }
+
+
+    }
+}
