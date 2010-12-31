c91e7761118db295da7d111567357a3ed04b8e4a
cdnicoll <cdnicoll@localhost>
2010-12-30 16:18:28 -0800

    Added a player class

---
+++ Games/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using BusinessLogic.Cards;
+
+namespace Games
+{
+    public class Player
+    {
+        public Hand PlayerHand { get; set; }
+        public String PlayerName { get; set; }
+        public int GamesPlayed { get; set; }
+        public int GamesWon { get; set; }
+
+        public Player(string name)
+        {
+            PlayerHand = new Hand();
+            PlayerName = name;
+            GamesPlayed = 0;
+            GamesWon = 0;
+        }
+    }
+}
